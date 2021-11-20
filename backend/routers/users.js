e005e6aee8e2a8dc431471537a25b417000e71d7
Yasin <adan.a.yasin@outlook.com>
2021-11-19 23:06:13 -0600

    Implemented CRUD operations

--- backend/routers/users.js
+++ backend/routers/users.js
@@ -5,31 +5,54 @@ const { request } = require("express");
 
 //Update user
 router.put("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.body.isAdmin) {
-      if (req.body.password) {
-        try {
-          const salt = await bcrypt.genSalt(10);
-          req.body.password = await bcrypt.hash(req.body.password, salt);
-        } catch (err) {
-          return res.status(500).json(err);
-        }
-      }
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
       try {
-        const user = await User.findByIdAndUpdate(req.params.id, {
-          $set: req.body,
-        });
-        res.status(200).json("Account has been updated");
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
       } catch (err) {
         return res.status(500).json(err);
       }
-    } else {
-      return res.status(403).json("You can update only your account!");
     }
-  });
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can update only your account!");
+  }
+});
 
 //delete a user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete({ _id: req.params.id });
+      res.status(200).json("Account has been deleted succesfully");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
+
 //get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updateAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //follow a user
+
 //unfollow a user
 
 router.get("/", (req, res) => {});
